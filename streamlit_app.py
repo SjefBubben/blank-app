3a8c48732edbeee5c9c905be3a911c1e53c0d3a6
SjefBubben <throwawaybubbernegaming@gmail.com>
2025-06-04 13:02:45 +0000

    test2

--- streamlit_app.py
+++ streamlit_app.py
@@ -537,9 +537,12 @@ st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ("🏠 Home", "📝 Input", "📊 Stats", "🚽 Motivation"))
 
 #Refresh og datepicker
-days = st.number_input("Days back", min_value=1, max_value=15, value=2)
-if st.button("🔄 Refresh Data"):
-    refresh_all(days)
+col1, col2 = st.columns([1, 1]) 
+with col1:
+    days = st.number_input("Days back", min_value=1, max_value=15, value=2)
+with col2:
+    if st.button("🔄 Refresh Data"):
+        refresh_all(days)
 
 
 if page == "🏠 Home":
